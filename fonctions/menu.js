1b643f7aa215bd108fe3b7fa80570a5e4cbc2a37
benjamin-edouard <130186004+benjamin-edouard@users.noreply.github.com>
2023-05-31 22:14:11 +0200

    Rajout d'une condition pour afficher le menu mobile ou desktop

--- fonctions/menu.js
+++ fonctions/menu.js
@@ -1,5 +1,13 @@
-document.getElementById('main-menu').style.display = 'none';
-document.getElementById('menu-icon').addEventListener('click', displayMenu);
+let screenWidth = screen.width;
+
+if(screenWidth < 800) {
+    document.getElementById('main-menu').style.display = 'none';
+    document.getElementById('menu-icon').addEventListener('click', displayMenu);
+} else {
+    document.getElementById('main-menu').style.display = 'block';
+    document.getElementById('menu-icon').style.display = 'none';
+}
+
 
 function displayMenu() {
     let mainMenu = document.getElementById('main-menu');
