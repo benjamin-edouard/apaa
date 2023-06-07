0abb0f32d9a3b1d314c1c61289bf974ca8464386
benjamin-edouard <130186004+benjamin-edouard@users.noreply.github.com>
2023-06-07 10:48:56 +0200

    Résolution de bugs mineurs concernant l'affichage dans la version desktop

--- fonctions/menu.js
+++ fonctions/menu.js
@@ -1,18 +1,20 @@
-let screenWidth = screen.width;
+menuOnResize();
+document.addEventListener('resize', menuOnResize);
 
-if(screenWidth < 800) {
-    document.getElementById('main-menu').style.display = 'none';
-    document.getElementById('menu-icon').addEventListener('click', displayMenu);
-} else {
-    document.getElementById('main-menu').style.display = 'block';
-    document.getElementById('menu-icon').style.display = 'none';
+function menuOnResize() {
+    let screenWidth = screen.width;
+
+    if (screenWidth < 800) {
+        document.getElementById('main-menu').style.display = 'none';
+        document.getElementById('menu-icon').addEventListener('click', displayMenu);
+    }
 }
 
 
 function displayMenu() {
     let mainMenu = document.getElementById('main-menu');
 
-    if(mainMenu.style.display === 'none') {
+    if (mainMenu.style.display === 'none') {
         mainMenu.style.display = 'block';
     } else {
         mainMenu.style.display = 'none';
